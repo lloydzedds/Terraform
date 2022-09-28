0e0646e2058d499d15a29da139d9efce54ed57e3
lloydzedds <lloydzedds@gmail.com>
2022-09-29 00:43:58 +0530

    instance file

---
+++ createinstance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+ aaccess_key =  "AKIATKU4CRCZCXPP4AER"
+ secret_key = "dRTaLeVeY5Nf/HJCGOV8Q2Aiob14MPeXIifDnuPa"
+ region = "eu-north-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-000e50175c5f86214"
+    instance_type = "t3.micro"
+  
+}
