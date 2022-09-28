de347538657099f5e91d88f65db7390bc28fae80
lloydzedds <lloydzedds@gmail.com>
2022-09-29 01:24:46 +0530

    dd

--- createinstance.tf
+++ createinstance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
  access_key = "AKIATKU4CRCZCXPP4AER"
  secret_key = "dRTaLeVeY5Nf/HJCGOV8Q2Aiob14MPeXIifDnuPa"
- region = "eu-north-1"
+ region = "ap-south-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-000e50175c5f86214"
-    instance_type = "t3.micro"
+    ami = "ami-0f2e255ec956ade7f"
+    instance_type = "t2.micro"
   
 }
